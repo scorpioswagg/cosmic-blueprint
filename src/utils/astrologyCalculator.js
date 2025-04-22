ea96acf48b22b9a3e8658f5bc0c13ff56a9914ac
Kyle <astro.butt84@gmail.com>
2025-04-22 03:21:38 -0700

    Create astrologyCalculator.js

---
+++ src/utils/astrologyCalculator.js
@@ -0,0 +1,18 @@
+export function calculateChartData({ name, birthDate, birthTime, birthCity }) {
+  return {
+    name,
+    sun: 'Aries',
+    rising: 'Libra',
+    southNode: 'Pisces',
+    northNode: 'Virgo',
+    karmicThemes: 'emotional escapism, sacrifice, spiritual wisdom',
+    soulMission: 'practical service, discernment, healing through routine',
+    saturnSign: 'Capricorn',
+    saturnHouse: '10',
+    venusSign: 'Taurus',
+    marsSign: 'Scorpio',
+    descendantSign: 'Aries',
+    chironSign: 'Gemini',
+    chironHouse: '3',
+  };
+}
