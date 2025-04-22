77f2c910ff0b72a9c2cb1ba9e51769c49798ab0b
scorpioswagg <astro.butt84@gmail.com>
2025-04-22 02:53:37 -0700

    Initial commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | 
+      <Link to="/report">Report</Link>
+    </nav>
+  );
+}
+
+export default NavBar;
